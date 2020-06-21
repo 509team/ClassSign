6e3b6be8176e15174dc7d521b8dc36461dfc54a0
xbwold <944382471@qq.com>
2020-06-21 17:38:37 +0800

    添加顶部状态栏,完成修改密码页面

--- app/src/main/java/com/fzn/classsign/activitys/UpdatePasswordActivity.java
+++ app/src/main/java/com/fzn/classsign/activitys/UpdatePasswordActivity.java
@@ -3,6 +3,7 @@ package com.fzn.classsign.activitys;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.fzn.classsign.R;
 
@@ -10,10 +11,14 @@ import com.fzn.classsign.R;
  * 修改密码
  */
 public class UpdatePasswordActivity extends AppCompatActivity {
+    TextView topTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_password);
+
+        topTitle=findViewById(R.id.tv_tsb_title);
+        topTitle.setText("修改密码");
     }
 }
