6bd471cabe12f347fe6679a6fd153311328f9362
xkbnb <49673333+xkbnb@users.noreply.github.com>
2020-06-23 15:11:30 +0800

    配置文件

---
+++ app/src/main/java/com/fzn/classsign/util/BaseResponseUtil.java
@@ -0,0 +1,40 @@
+package com.fzn.classsign.util;
+
+public class BaseResponseUtil<T> {
+    private int code;
+    private int statusCode;
+    private String msg;
+    private T data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
