5a16e598fc83a5919a87b3b64d54dc729108097f
xkbnb <49673333+xkbnb@users.noreply.github.com>
2020-06-25 20:49:15 +0800

    加入班级全过程

--- app/src/main/java/com/fzn/classsign/activitys/JoinClassActivity.java
+++ app/src/main/java/com/fzn/classsign/activitys/JoinClassActivity.java
@@ -12,10 +12,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.fzn.classsign.R;
+import com.fzn.classsign.asynctask.student.ClassAdd;
+import com.fzn.classsign.entity.Token;
 import com.yzq.zxinglibrary.android.CaptureActivity;
 import com.yzq.zxinglibrary.common.Constant;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 加入班级
@@ -31,6 +37,8 @@ public class JoinClassActivity extends AppCompatActivity implements View.OnClick
 
     /*权限*/
     private final int REQUEST_CODE_SCAN = 0x0000;
+    /*二维码扫描结果*/
+    private String scanResult;
 
 
     @Override
@@ -51,21 +59,29 @@ public class JoinClassActivity extends AppCompatActivity implements View.OnClick
 
     @Override
     public void onClick(View v) {
-        if(v.getId() ==  R.id.iv_jc_scan){
+        if (v.getId() == R.id.iv_jc_scan) {
             //动态申请相机权限、读取文件权限；
             if (ContextCompat.checkSelfPermission(JoinClassActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(JoinClassActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(JoinClassActivity.this, new String[]{Manifest.permission.CAMERA , Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
-            }else {
+                ActivityCompat.requestPermissions(JoinClassActivity.this, new String[]{Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            } else {
                 //扫码返回结果
                 Intent intent = new Intent(JoinClassActivity.this, CaptureActivity.class);
                 startActivityForResult(intent, REQUEST_CODE_SCAN);
-                //TODO
-                //判断二维码信息是否一致
             }
         }
-        if(v.getId() ==  R.id.bt_jc_joinclass){
-            //TODO
-            //加入班级
+        if (v.getId() == R.id.bt_jc_joinclass) {
+            String sixcode = et_jc_sixcode.getText().toString();
+            if (sixcode.length() != 6) {
+                Toast.makeText(JoinClassActivity.this, "签到码不符合要求", Toast.LENGTH_SHORT).show();
+            } else {
+                Map<String, String> head = new HashMap<>();
+                head.put("Authorization", "Bearer " + Token.token);
+                Map<String, Object> params = new HashMap<>();
+                params.put("joinCode", sixcode);
+                new ClassAdd<Boolean>(head, null, params, JoinClassActivity.this)
+                        .post()
+                        .execute();
+            }
         }
     }
 
@@ -75,10 +91,24 @@ public class JoinClassActivity extends AppCompatActivity implements View.OnClick
         // 扫描二维码/条码回传
         if (requestCode == REQUEST_CODE_SCAN && resultCode == RESULT_OK) {
             if (data != null) {
-
-                String content = data.getStringExtra(Constant.CODED_CONTENT);
-                et_jc_sixcode.setText("扫描结果为：" + content);
+                scanResult = data.getStringExtra(Constant.CODED_CONTENT);
+                scanResult = scanResult.substring(scanResult.indexOf("-") + 1);
+                if (scanResult.length() == 6) {
+                    Map<String, String> head = new HashMap<>();
+                    head.put("Authorization", "Bearer " + Token.token);
+                    Map<String, Object> params = new HashMap<>();
+                    params.put("joinCode", scanResult);
+                    new ClassAdd<Boolean>(head, null, params, JoinClassActivity.this)
+                            .post()
+                            .execute();
+                } else {
+                    Toast.makeText(JoinClassActivity.this, "扫描结果与实际签到码不符！请重试！", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(JoinClassActivity.this, "扫描结果为空！", Toast.LENGTH_SHORT).show();
             }
+        } else {
+            Toast.makeText(JoinClassActivity.this, "扫描出错，请重新扫描！", Toast.LENGTH_SHORT).show();
         }
     }
 
--- app/src/main/java/com/fzn/classsign/asynctask/student/ClassAdd.java
+++ app/src/main/java/com/fzn/classsign/asynctask/student/ClassAdd.java
@@ -1,5 +1,10 @@
 package com.fzn.classsign.asynctask.student;
 
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.fzn.classsign.activitys.ClassHomePageStudentActivity;
 import com.fzn.classsign.asynctask.base.CustomAsyncTask;
 import com.fzn.classsign.constant.RequestConstant;
 
@@ -7,18 +12,28 @@ import java.util.Map;
 
 /**
  * 令牌刷新接口
+ *
  * @param <T>
  */
 public class ClassAdd<T> extends CustomAsyncTask<T> {
-    public ClassAdd(Map headers, Map body, Map params){
-        super(headers,body,params, RequestConstant.URL.STU_CLASS_ADD);
+    private Context context;
 
+    public ClassAdd(Map headers, Map body, Map params, Context context) {
+        super(headers, body, params, RequestConstant.URL.STU_CLASS_ADD);
+        this.context = context;
     }
 
     @Override
     protected void onPostExecute(String s) {
         ResponseResultJson<Boolean> temp = (ResponseResultJson<Boolean>) getResponse(s);
-
+        int code = temp.getCode();
+        if (code == 200) {
+            Toast.makeText(context, "加入班级成功", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(context, ClassHomePageStudentActivity.class);
+            context.startActivity(intent);
+        } else {
+            Toast.makeText(context, "加入班级失败", Toast.LENGTH_SHORT).show();
+        }
         Boolean data = temp.getData();
 
         super.onPostExecute(s);
