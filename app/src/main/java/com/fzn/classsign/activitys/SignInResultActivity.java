852630b83daf9efc069288815a36d53443945437
xkbnb <49673333+xkbnb@users.noreply.github.com>
2020-06-25 20:44:22 +0800

    学生端签到全过程

--- app/src/main/java/com/fzn/classsign/activitys/SignInResultActivity.java
+++ app/src/main/java/com/fzn/classsign/activitys/SignInResultActivity.java
@@ -6,17 +6,44 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.fzn.classsign.R;
 
 /**
  * 签到成功
  */
-public class SignInResultActivity extends AppCompatActivity {
+public class SignInResultActivity extends AppCompatActivity implements View.OnClickListener {
+    private ImageView iv_sir_signresultimage;
+    private TextView tv_sir_signresulttext;
+    private Button bt_sir_return;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_result);
+
+        iv_sir_signresultimage = findViewById(R.id.iv_sir_signresultimage);
+        tv_sir_signresulttext = findViewById(R.id.tv_sir_signresulttext);
+        bt_sir_return = findViewById(R.id.bt_sir_return);
+        bt_sir_return.setOnClickListener(this);
+
+        Intent intent = getIntent();
+        if (intent.getStringExtra("RESULT").equals("SUCCESS")) {
+            iv_sir_signresultimage.setImageResource(R.drawable.sign_success);
+            tv_sir_signresulttext.setText("签到成功");
+        } else if (intent.getStringExtra("RESULT").equals("ABNORMAL")) {
+            iv_sir_signresultimage.setImageResource(R.drawable.sign_failed);
+            tv_sir_signresulttext.setText("签到异常");
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.bt_sir_return) {
+            Intent intent = new Intent(SignInResultActivity.this, SignInActivity.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/fzn/classsign/asynctask/student/SignIn.java
+++ app/src/main/java/com/fzn/classsign/asynctask/student/SignIn.java
@@ -1,5 +1,10 @@
 package com.fzn.classsign.asynctask.student;
 
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.fzn.classsign.activitys.SignInResultActivity;
 import com.fzn.classsign.asynctask.base.CustomAsyncTask;
 import com.fzn.classsign.constant.RequestConstant;
 
@@ -7,19 +12,40 @@ import java.util.Map;
 
 /**
  * 令牌刷新接口
+ *
  * @param <T>
  */
 public class SignIn<T> extends CustomAsyncTask<T> {
-    public SignIn(Map headers, Map body, Map params){
-        super(headers,body,params, RequestConstant.URL.SIGN_LIST_SIGNIN);
+    private Context context;
 
+    public SignIn(Map headers, Map body, Map params, Context context) {
+        super(headers, body, params, RequestConstant.URL.STU_SIGN_SIGNIN);
+        this.context = context;
     }
 
     @Override
     protected void onPostExecute(String s) {
         ResponseResultJson<Boolean> temp = (ResponseResultJson<Boolean>) getResponse(s);
-
-        Boolean data = temp.getData();
+        int code = temp.getCode();
+        if (code == 200) {
+            int statusCode = temp.getStatusCode();
+            if (statusCode == 201) {
+                Toast.makeText(context, "签到成功", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(context, SignInResultActivity.class);
+                intent.putExtra("RESULT", "SUCCESS");
+                context.startActivity(intent);
+            } else if (statusCode == 202) {
+                Toast.makeText(context, "签到异常", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(context, SignInResultActivity.class);
+                intent.putExtra("RESULT", "ABNORMAL");
+                context.startActivity(intent);
+            }
+        } else if (code == 203) {
+            String text = temp.getAddtionalInfo();
+            Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(context, "签到失败！", Toast.LENGTH_SHORT).show();
+        }
 
         super.onPostExecute(s);
     }
